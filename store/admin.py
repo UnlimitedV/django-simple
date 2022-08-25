d4b2f49febe5799d01b10227b0a8f4bd991efa2b
Vahid <kingvm8@gmail.com>
2022-08-25 11:29:23 +0430

    computed columns

--- store/admin.py
+++ store/admin.py
@@ -3,6 +3,12 @@ from store.models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'unit_price', 'collection']
-    sortable_by = ['title']
-    list_per_page = 5
+    list_display = ['id', 'title', 'unit_price', 'inventory_status']
+    # sortable_by = ['title']
+    list_per_page = 15
+    
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory % 2 == 0:
+            return 'even'
+        return 'odd'
