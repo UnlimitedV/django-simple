4c28f2a35522f837944814537bd6ba82cf7275df
Vahid <kingvm8@gmail.com>
2022-08-25 11:14:36 +0430

    Admin Customization

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from store.models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'unit_price', 'collection']
+    sortable_by = ['title']
+    list_per_page = 5
