9db42147c24089f184116f23c5da2515b32b3eb0
Vahid <kingvm8@gmail.com>
2022-08-27 12:34:37 +0430

    fix

--- store/views.py
+++ store/views.py
@@ -8,12 +8,18 @@ from .serializers import ProductSerializer, CollectionSerializer
 from .models import Product, Collection
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request: Request):
-    queryset = Product.objects.select_related('collection').all()
-    serial = ProductSerializer(queryset, many=True, context={'request':request})
-    return Response(serial.data)
-
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serial = ProductSerializer(queryset, many=True, context={'request':request})
+        return Response(serial.data)
+    elif request.method == 'POST':
+        serial = ProductSerializer(data=request.data, context={'request':request})
+        serial.is_valid(raise_exception=True)
+        serial.save()
+        return Response(serial.validated_data)
+    
 
 @api_view()
 def product_detail(request: Request, pk):
@@ -27,3 +33,4 @@ def collection_detail(request, pk):
     collection = get_object_or_404(Collection, pk=pk)
     serial = CollectionSerializer(collection)
     return Response(serial.data)
+
