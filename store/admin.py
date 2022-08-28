f3ead6875f7f0951ce8aee40b13442fda1e4aa6d
Vahid <kingvm8@gmail.com>
2022-08-28 14:36:33 +0430

    admin custom filter

--- store/admin.py
+++ store/admin.py
@@ -1,16 +1,30 @@
 from django.contrib import admin
-from store.models import Collection, Product, Order
-from django.db.models import Count
+from store.models import Collection, Product, Order, Customer
+from django.db.models import Count, F
+from django.db.models.query import QuerySet
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [('%2==0', 'even'), ('%2==1', 'odd')]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '%2==0':
+            return queryset.annotate(even=F('inventory')%2).filter(even=False)
+        elif self.value() == '%2==1':
+            return queryset.annotate(odd=F('inventory')%2).filter(odd=True)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'unit_price', 'inventory_status', 'collection_title']
-    # sortable_by = ['title']
+    list_display = ['id', 'title', 'unit_price', 'inventory', 'inventory_status', 'collection_title']
     list_per_page = 100
     list_select_related = ['collection']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     
-
     def collection_title(self, product):
         return product.collection.title
 
@@ -19,7 +33,6 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory % 2 == 0:
             return 'even'
         return 'odd'
-
 
 
 @admin.register(Order)
@@ -31,7 +44,6 @@ class OrderAdmin(admin.ModelAdmin):
     def customer_name(self, order):
         fullname = order.customer.first_name + ' ' + order.customer.last_name
         return fullname
-
 
 
 @admin.register(Collection)
@@ -46,3 +58,13 @@ class CollectionAdmin(admin.ModelAdmin):
         return super().get_queryset(request).annotate(
             product_count = Count('product')
         )
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+    sortable_by = ['first_name', 'last_name']
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name']
