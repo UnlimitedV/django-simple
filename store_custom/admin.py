ad76919b81403f0707742b0804df5877b496c559
Vahid <kingvm8@gmail.com>
2022-08-29 19:47:14 +0430

    tagitem extra fields fix

--- store_custom/admin.py
+++ store_custom/admin.py
@@ -8,6 +8,9 @@ from tags.models import TaggedItem
 class TagInline(GenericTabularInline):
     model = TaggedItem
     autocomplete_fields = ['tag']
+    extra = 0
+    min_num = 1
+    max_num = 5
 
 
 class CustomProductAdmin(ProductAdmin):
