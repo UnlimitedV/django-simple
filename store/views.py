cc14e45648b1c93949e28e291b0470fc829a30e4
Vahid <kingvm8@gmail.com>
2022-09-08 16:10:37 +0430

    create cart

--- store/views.py
+++ store/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
 from rest_framework import status
 from .filters import ProductFilter
-from .models import Collection, OrderItem, Product, Review
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from .models import Collection, OrderItem, Product, Review, CartItem, Cart
+from .serializers import CartSerializer, CollectionSerializer, ProductSerializer, ReviewSerializer
 from .pagination import DefaultPagination
 
 
@@ -48,3 +49,8 @@ class ReviewViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
+
+
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
